b32a5fb84e7d54e37f1df36b48e5f0a3528702ae
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-07-27 16:59:03 +0200

    started relationships with tables

--- database/migrations/2021_07_27_142900_add_category_id_to_posts_table.php
+++ database/migrations/2021_07_27_142900_add_category_id_to_posts_table.php
@@ -14,7 +14,11 @@ class AddCategoryIdToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->onDelete('set null');
         });
     }
 
@@ -26,7 +30,8 @@ class AddCategoryIdToPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropForeign('posts_category_id_foreing');
+            $table->dropColumn('category_id');
         });
     }
 }
